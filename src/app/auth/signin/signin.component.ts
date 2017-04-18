d3ace49bb67f67164b52fdb4efd9754857bba988
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-04-18 16:27:38 -0700

    Call api for sign in works

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,20 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+// import { Component, OnInit } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+//
+// @Component({
+//   selector: 'app-signin',
+//   templateUrl: './signin.component.html',
+//   styleUrls: ['./signin.component.css']
+// })
+// export class SigninComponent implements OnInit {
+//
+//   constructor() { }
+//
+//   ngOnInit() {
+//   }
+//
+//   onSignIn(form: NgForm) {
+//     const email = form.value.email;
+//     const password = form.value.password;
+//   }
+// }
 
+
+import { Component, OnInit, Inject } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Http, Response} from '@angular/http';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
+  signinForm: FormGroup;
+  user: Object;
+  constructor(private http: Http, @Inject(FormBuilder) fb: FormBuilder) {
+      this.signinForm = fb.group({
+          email: '',
+          password: ''
+      })
+   }
 
   ngOnInit() {
   }
-  
-  onSignIn(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.password;
+  onSignin(values: string): any {
+    const url = 'https://cheeswhiz.herokuapp.com/api/user/login';
+    console.log('values', values)
+    this.http.post(url, values)
+      .subscribe((res: Response) => {
+        this.user = res.json();
+        console.log('user', this.user)
+      })
   }
+
 }
